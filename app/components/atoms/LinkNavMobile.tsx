b2736de6f523741caa4f08576ce18723c8fad479
Pablo Zallio <pablozalliodev@gmail.com>
2026-09-25 12:46:55 +0200

    [STYLE] - Correcting accesibility

--- app/components/atoms/LinkNavMobile.tsx
+++ app/components/atoms/LinkNavMobile.tsx
@@ -1,11 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-export default function LinkNavMobile({ href, label, number, onClick }: { href: string, label: string, number: string, onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void }) {
-      return (
-            <Link href={href} className=' flex items-center justify-between min-h-16 border-b border-border font-archivo-black font-black text-xl uppercase '  onClick={onClick}>
-                  {label}
-                  <p className=" font-barlow text-[10px] text-primary  ">{number}</p>
-            </Link>
-      )
+export default function LinkNavMobile({
+  href,
+  label,
+  number,
+  onClick,
+}: {
+  href: string;
+  label: string;
+  number: string;
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+}) {
+  return (
+    <Link
+      href={href}
+      aria-label="Abrir menú de navegación"
+      className=" flex items-center justify-between min-h-16 border-b border-border font-archivo-black font-black text-xl uppercase "
+      onClick={onClick}
+    >
+      {label}
+      <p className=" font-barlow text-[10px] text-primary  ">{number}</p>
+    </Link>
+  );
 }
--- app/components/organisms/MenuMobile.tsx
+++ app/components/organisms/MenuMobile.tsx
@@ -12,7 +12,7 @@ export default function MenuMobile({ open, setOpen }: { open: boolean, setOpen: 
                   }`}>
                   <aside className={`fixed top-0 right-0 z-200 flex flex-col gap-4 w-full max-w-sm py-20 px-5 bg-background h-screen border-l border-primary transition-transform duration-200 ease-in-out ${open ? 'translate-x-0' : 'translate-x-full'
                         }`}>
-                        <button className=" absolute top-4 right-4 " onClick={() => setOpen(!open)} ><X size={16} className=' text-foreground ' /></button>
+                        <button aria-label="Cerrar menú de navegación" className=" absolute top-4 right-4 " onClick={() => setOpen(!open)} ><X size={16} className=' text-foreground ' /></button>
                         <LogoMenuMobile />
                         <NavMobile />
                         <LinkReserveButton full >reservar por whatsapp</LinkReserveButton>
